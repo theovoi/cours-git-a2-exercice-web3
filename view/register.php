4b65829ef7e0d557dfe1d7139bbeceed592eb844
Elies Makhlouf <elies.makhlouf@gmail.comb@domain.ltd>
2015-10-06 17:20:16 +0200

    Ajout des balises name

--- view/register.php
+++ view/register.php
@@ -12,22 +12,22 @@
 				<form method="POST" action="register.php">
 					<div class="form-input">
 						<span class="username">
-							<input type="text" placeholder="Username" name="#">
+							<input type="text" placeholder="Username" name="username">
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="email">
-							<input type="text" placeholder="Email" name="#">
+							<input type="text" placeholder="Email" name="email">
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<input type="password" placeholder="Password" name="#">
+							<input type="password" placeholder="Password" name="password">
 						</span>
 					</div>
 
 					<div class="form-submit">
-						<input type="submit" value="Je m'inscris !" name="#">
+						<input type="submit" value="Je m'inscris !">
 					</div>
 					<p class="account">Vous avez un compte ? <a href="login.php">Connectez-vous !</a></p>
 				</form>
